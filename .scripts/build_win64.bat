6faadcea6c460a4d7eae0ecd7a58e45d53c93091
Jeremy Sapsed <jeremy@sapsed.co.uk>
2021-08-21 16:24:24 +0100

    Updated build scripts

--- .scripts/build_win64.bat
+++ .scripts/build_win64.bat
@@ -1,1 +1,1 @@
-"C:\Program Files\Unity\Hub\Editor\2021.1.17f1\Editor\Unity.exe" -batchmode -quit -nographics -projectPath ./Game/ -executeMethod Editor.BuildHelper.PerformBuildWin64 -logFile build.log
+"C:\Program Files\Unity\Hub\Editor\2021.1.17f1\Editor\Unity.exe" -batchmode -quit -projectPath ./Game/ -executeMethod Editor.BuildHelper.PerformBuildWin64 -logFile build.log
