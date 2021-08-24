1cddffda0a0aecd48e7e54b630eabf9b3d978f7e
Jeremy Sapsed <jeremy@sapsed.co.uk>
2021-08-24 18:40:07 +0100

    Added walking animation + IK

--- Game/Assets/Scripts/Controllers/MechController.cs
+++ Game/Assets/Scripts/Controllers/MechController.cs
@@ -7,16 +7,22 @@ using Weapons;
 [RequireComponent(typeof(CharacterController))]
 [RequireComponent(typeof(WeaponLoadout))]
 [RequireComponent(typeof(DamageSystem))]
+[RequireComponent(typeof(Animator))]
 public class MechController : UnitController
 {
     private CharacterController _characterController;
+    private Animator _animator;
 
     private Vector2 _positionInput = Vector2.zero;
+
+    [SerializeField]
+    private float _animationSpeedScale = 0.25f;
 
     protected override void Awake()
     {
         base.Awake();
         _characterController = GetComponent<CharacterController>();
+        _animator = GetComponent<Animator>();
     }
 
     protected override void FixedUpdate()
@@ -27,6 +33,20 @@ public class MechController : UnitController
         move.y = -9.81f;
         transform.Rotate(Vector3.up, _positionInput.x * _turnSpeed, Space.World);
         _characterController.Move(move * Time.deltaTime);
+        
+        _animator.speed = _characterController.velocity.magnitude * _animationSpeedScale;
+        if (_positionInput.sqrMagnitude > 0)
+        {
+            if (_positionInput.y == 0)
+            {
+                _animator.speed = 1f;
+            }
+            _animator.SetBool("Walking", true);
+        }
+        else
+        {
+            _animator.SetBool("Walking", false);
+        }
     }
 
     public void Move(InputAction.CallbackContext context)
